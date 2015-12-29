993d75330b6e4ecaf7c918dcd0bad968677170bc
Ilya Doroshenko <divon.step@gmail.com>
2015-12-29 09:23:27 +0600

    change db schema

--- db/mysql/refresh.sql
+++
@@ -1,7 +0,0 @@
-TRUNCATE TABLE associations_history;
-TRUNCATE TABLE excess_image_history;
-TRUNCATE TABLE grammar_history;
-TRUNCATE TABLE maze_history;
-TRUNCATE TABLE word_search_history;
-TRUNCATE TABLE quest;
-UPDATE users SET TASK_ID = 0;
